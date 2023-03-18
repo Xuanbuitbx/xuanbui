8d44f123241b6acfdffbd372ce3a1013a346e407
Xuân Bùi <tbx.deg@gmail.com>
2023-03-19 00:28:13 +0700

    reup file

---
+++ Mindx- Lesson/lesson 7/app.js
@@ -0,0 +1,49 @@
+// console.log("wellcome to javascript");
+// console.log("hello");
+// console.log("this is test text");
+// console.log("đây là lỗi");
+// // tên biến bắt đầu bằng chữ cái, _, $,
+
+// //trongdattienbien có thể sử dụng số  
+// let name$1; 
+
+// name="giá trị mới của name"; 
+// console.log(name); 
+// let age = 10; 
+
+
+// const hello="cần lao vi tiên thủ"; //hằng số là biến số không thể thay đổi 
+// let something = "a";
+// let SOMETHING = "B"
+
+// let age = 28;
+// let weight = 71;
+// let height = 170;
+// let breast1 = 90;
+
+// // string chuỗi 
+// let slogan = "cần lao vi tiên thủ";
+// let name = "chinh đỗ";
+// let addtress = "Hanoi";
+// let phone = "+84432423423";
+
+// // kiểu boolean ()
+
+// let hasGrilfriend = false;
+// let isRick = fail;
+
+// //kiểu null
+// let hasgrilfriend = null;
+// let money = null;
+
+
+// //kiểu undefined
+// let future;
+// let wiffe;
+
+let   a = 10;
+let  b = 10;
+let  c = 30;
+let d ='40'
+console.log (a+b+c+d) //toán tử so sánh ra một giá trị boolean true or false
+                        
