f3c725c3d36546583bbfa8e762670a3a2f74b371
Xuân Bùi <tbx.deg@gmail.com>
2023-03-29 14:59:48 +0700

    bai 8

---
+++ Bai tap/Ex lesson 8/app.js
@@ -0,0 +1,39 @@
+// let a = 'hello';
+// let b = 10 ;
+// // nhập giá trị cho biến
+
+// let input = prompt("Hãy nhập vào đây để gán giá trị cho biến") //nhập thông báo 
+// console.log("bạn vừa nhập vào " + input); // số thì có màu còn string là màu trắng
+
+
+// let x =Number( prompt('nhập số X'));
+// let y =Number ( prompt('nhập số Y'));
+// let z = x + y;
+// console.log(z);
+
+
+// let a = "hello world"
+
+// {
+//     let x = 10;
+//     let y = 40;
+//     console.log(x + y);
+// }
+
+// let name =  "chinhdo";
+// {   //bên trong khối lệnh
+//     let name = "someone Else";
+//     let age = "28" ;
+//     {
+//         let address = "thai binh";
+//         // dùng được age ở đây
+//     }
+//     console.log(name);
+//     //không dùng được ở đây
+// }
+
+for ( let count = 1; count<=500; count++ );
+
+{
+console.log(count + "I love you");
+}
--- Mindx- Lesson/lesson 7/app.js
+++ Mindx- Lesson/lesson 7/app.js
@@ -1,49 +1,166 @@
-// console.log("wellcome to javascript");
-// console.log("hello");
-// console.log("this is test text");
-// console.log("đây là lỗi");
-// // tên biến bắt đầu bằng chữ cái, _, $,
+// // console.log("wellcome to javascript");
+// // console.log("hello");
+// // console.log("this is test text");
+// // console.log("đây là lỗi");
+// // // tên biến bắt đầu bằng chữ cái, _, $,
 
-// //trongdattienbien có thể sử dụng số  
-// let name$1; 
+// // //trongdattienbien có thể sử dụng số  
+// // let name$1; 
 
-// name="giá trị mới của name"; 
-// console.log(name); 
-// let age = 10; 
-
-
-// const hello="cần lao vi tiên thủ"; //hằng số là biến số không thể thay đổi 
-// let something = "a";
-// let SOMETHING = "B"
-
-// let age = 28;
-// let weight = 71;
-// let height = 170;
-// let breast1 = 90;
-
-// // string chuỗi 
-// let slogan = "cần lao vi tiên thủ";
-// let name = "chinh đỗ";
-// let addtress = "Hanoi";
-// let phone = "+84432423423";
-
-// // kiểu boolean ()
-
-// let hasGrilfriend = false;
-// let isRick = fail;
-
-// //kiểu null
-// let hasgrilfriend = null;
-// let money = null;
+// // name="giá trị mới của name"; 
+// // console.log(name); 
+// // let age = 10; 
 
 
-// //kiểu undefined
-// let future;
-// let wiffe;
+// // const hello="cần lao vi tiên thủ"; //hằng số là biến số không thể thay đổi 
+// // let something = "a";
+// // let SOMETHING = "B"
 
-let   a = 10;
-let  b = 10;
-let  c = 30;
-let d ='40'
-console.log (a+b+c+d) //toán tử so sánh ra một giá trị boolean true or false
+// // let age = 28;
+// // let weight = 71;
+// // let height = 170;
+// // let breast1 = 90;
+
+// // // string chuỗi 
+// // let slogan = "cần lao vi tiên thủ";
+// // let name = "chinh đỗ";
+// // let addtress = "Hanoi";
+// // let phone = "+84432423423";
+
+// // // kiểu boolean ()
+
+// // let hasGrilfriend = false;
+// // let isRick = fail;
+
+// // //kiểu null
+// // let hasgrilfriend = null;
+// // let money = null;
+
+
+// // //kiểu undefined
+// // let future;
+// // let wiffe;
+
+// // let   a = 10;
+// // let  b = 10;
+// // let  c = 30;
+// // let d ='40'
+// // console.log (a+b+c+d) //toán tử so sánh ra một giá trị boolean true or false
                         
+
+
+// // let name;
+// // name = "giá trị của name" ; // giá trị của biến
+// // console.log(name); // hiển thị giá trị của biến
+
+// // let age = 32; //khai báo giá trị biến;
+
+// // let x = 3;
+// //  x += 2;
+// //  console.log (x);
+
+// // let input = prompt("hãy nhập vào đây để gán giá trị cho biến ") ;// input : nhập prompt : thông báo cho người viết vào 
+// // console.log("bạn vừa nhập vào " + input );
+
+// // let rank = prompt('nhập vào phân loại');
+// // if (rank == 1) {
+// //     console.log ('ngon') } 
+// // else if (rank == 2) {
+// //     console.log ('9.5 điểm') }
+
+
+// // let text = prompt('nhập độ dài của chuỗi ');
+// // if(text.length < 8);
+// //     {console.log('chuỗi ngắn quá');} 
+// // else {consolog ('chuỗi này được')} 
+
+// // cách khai báo mảng
+// let names = [
+//     "Chinh do",
+//     " Ngoc Trinh",
+//     " chi pu",
+//     " đỗ quyên";
+    
+// ];
+// console.log (names);
+// // truy xuất giá trị 1 phần tử bât kỳ trong mảng
+// // thay đổi giá trị bất kỳ trong mảng ?
+// console.log()
+
+// names(0)="doi ten";
+
+
+
+// let array1 = ['a', 'b', 'C'];
+// let array2 = ['a', 'b', 'C'];
+// console.log(typeof (array1));
+// console.log(array1 + array2) //string+string -ghép. Chương trình tự động ghép kiểu khi tự động thực hiện phép cộng
+// console.log(array1 == array2 );
+// array1[0] = "HELLO"
+
+
+// DUYỆT NMANG
+
+//cách 1;
+// for (let i = 0; i <= names.length; i++){
+//     console.log(names[i])
+//     khai báo ra ngoài, vượt qua từng phần tử 
+// }
+// let min = Number[0];
+// for (let i = 0; i< Number.length; i++) {
+//     const Number = Number[i];
+//     if{ min > Number}
+// }
+
+//
+//thêm vào đầu mảng
+// Number.unshift(100, 1000, -4);
+// Number.push(-2,-10 );
+// console.log(Number);
+
+// let deletefirs
+
+
+// console.log("hello wordl");
+// {
+//     let x = 1;
+//     console.log(x);
+// }
+
+// let x = 1;
+// {
+//     let y = 2;
+//     console.log(y); //2
+//     console.log(x);  //1
+// }
+// console.log(y);
+// let x = 2;
+// if (x % 2 === 0); //% là lấy số dư === bằng giá trị số 
+// console.log("x is even");
+
+
+// if () {
+    
+// } else {
+//     console.log("giá trị này đúng");
+// }
+
+// if
+
+// SHOW CÁC SỐ TỪ 1 ĐẾN 5
+// for (var i = 1; i <=5; i++ )
+// {console.log(i);}
+
+// tổng từ 1 đến 5
+// let sum = 0
+// for (let i = 1; i <= 5 ; i++)
+// {sum += i}
+// console.log(sum);
+
+// ===============================
+// bài tập 
+
+
+let a = 10;
+let b = 5;
+let c = '1'
